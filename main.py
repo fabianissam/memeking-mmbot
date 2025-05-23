d8ccf3ddc65fbafc7954989260ea3fde4a854e44
Fabian Abdallah <fabian.abdallah98@googlemail.com>
2025-05-23 12:17:01 +0200

    Erstellung vom Meme des Monats

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ from mattermostdriver import Driver
 import datetime
 from dateutil.relativedelta import relativedelta
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 ALLOWED_EMOJIS = ['kekw','kekl']
 
-driver = Driver({'token':os.environ.get('MM_TOKEN'),'scheme':'http','port':8065,'url':'localhost','base_path':'/api/v4'})
+driver = Driver({'token':os.getenv('MM_TOKEN'),'scheme':os.getenv('MM_SCHEME'),'port':int(os.getenv('MM_PORT')),'url':os.getenv('MM_HOST'),'base_path':os.getenv('MM_BASE_PATH')})
 
 driver.login()
 
 team_id = driver.teams.get_user_teams('me')[0]['id']
+team_name = driver.teams.get_user_teams('me')[0]['name']
 
 channel_id = driver.channels.get_channel_by_name(team_id=team_id, channel_name='OT-Wissensaustausch')['id']
 
-min_date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(months=1) # datetime.datetime(2025, 5, 1)
+min_date = datetime.datetime(2025, 5, 1)#datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(months=1) # datetime.datetime(2025, 5, 1)
 since_timestamp = int(min_date.timestamp() * 1000)  # ms
 
 posts = driver.posts.get_posts_for_channel(
@@ -23,7 +28,11 @@ posts = driver.posts.get_posts_for_channel(
 
 user_reaction_map = {}
 
+best_meme = None
+reaction_count = 0
+
 for post in posts['posts']:
+    allowed_reaction_count=0
     # blacklist for users that already reacted on the post with another emoji
     reaction_user_blacklist= []
      
@@ -36,9 +45,15 @@ for post in posts['posts']:
         continue
     # iterate all reactions for a post
     for reaction in posts['posts'][post]['metadata']['reactions']:
-        if reaction['emoji_name'] in ALLOWED_EMOJIS and user_id not in reaction_user_blacklist:
+        if reaction['emoji_name'] in ALLOWED_EMOJIS and reaction['user_id'] not in reaction_user_blacklist:
+            allowed_reaction_count += 1
             reaction_user_blacklist.append(user_id)
             user_reaction_map[user_id] += 1
+            
+    if allowed_reaction_count > reaction_count:
+        reaction_count = allowed_reaction_count
+        best_meme = post
+
     
 user_reaction_map = dict(sorted(user_reaction_map.items(), key=lambda item: item[1], reverse=True))
 print(user_reaction_map)
@@ -51,6 +66,9 @@ for user in user_reaction_map:
 message += '\nHerzlichen Glückwunsch an '
 message += driver.users.get_user(list(user_reaction_map.keys())[0])['username'] 
 message += ' du bist der Memeking diesen Monats!'
+message += '\n\nHier ist das Meme des Monats:\n\n'
+message += os.getenv("MM_SCHEME")+'://'+os.getenv('MM_HOST')+':' +os.getenv("MM_PORT")+ '/'+ team_name + '/pl/' + best_meme
+
 
 driver.posts.create_post(options={'channel_id': channel_id, 'message': message})
 
